ff7c5ba78282c9cca5d40344d55a5ef526bc88af
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2025-12-27 01:05:24 +0200

    chore: add tests for duration cast

---
+++ tests/temporal/duration_tests.cpp
@@ -0,0 +1,44 @@
+#include "latertemporal.h"
+#include <gtest/gtest.h>
+
+using namespace Later::Temporal;
+
+TEST(DurationCast, Seconds) {
+    EXPECT_EQ(duration_cast<Seconds>(Minutes{2}), Seconds{120});
+    EXPECT_EQ(duration_cast<Seconds>(Hours{2}), Seconds{7200});
+    EXPECT_EQ(duration_cast<Seconds>(Days{2}), Seconds{172800});
+    EXPECT_EQ(duration_cast<Seconds>(Weeks{2}), Seconds{1209600});
+}
+
+TEST(DurationCast, Minutes) {
+    EXPECT_EQ(duration_cast<Minutes>(Seconds{120}), Minutes{2});
+    EXPECT_EQ(duration_cast<Minutes>(Hours{2}), Minutes{120});
+    EXPECT_EQ(duration_cast<Minutes>(Days{2}), Minutes{2880});
+    EXPECT_EQ(duration_cast<Minutes>(Weeks{2}), Minutes{20160});
+}
+
+TEST(DurationCast, Hours) {
+    EXPECT_EQ(duration_cast<Hours>(Seconds{7200}), Hours{2});
+    EXPECT_EQ(duration_cast<Hours>(Minutes{120}), Hours{2});
+    EXPECT_EQ(duration_cast<Hours>(Days{2}), Hours{48});
+    EXPECT_EQ(duration_cast<Hours>(Weeks{2}), Hours{336});
+}
+
+TEST(DurationCast, Days) {
+    EXPECT_EQ(duration_cast<Days>(Seconds{172800}), Days{2});
+    EXPECT_EQ(duration_cast<Days>(Minutes{2880}), Days{2});
+    EXPECT_EQ(duration_cast<Days>(Hours{48}), Days{2});
+    EXPECT_EQ(duration_cast<Days>(Weeks{2}), Days{14});
+}
+
+TEST(DurationCast, Weeks) {
+    EXPECT_EQ(duration_cast<Weeks>(Seconds{1209600}), Weeks{2});
+    EXPECT_EQ(duration_cast<Weeks>(Minutes{20160}), Weeks{2});
+    EXPECT_EQ(duration_cast<Weeks>(Hours{336}), Weeks{2});
+    EXPECT_EQ(duration_cast<Weeks>(Days{14}), Weeks{2});
+}
+
+TEST(DurationCast, Calendar) {
+    EXPECT_EQ(duration_cast<Months>(Years{2}), Months{24});
+    EXPECT_EQ(duration_cast<Years>(Months{24}), Years{2});
+}
