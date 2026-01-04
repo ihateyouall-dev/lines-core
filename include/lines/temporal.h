b48e24f05de92b28815b90fa0bd98142f19b8120
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-01-04 15:37:55 +0200

    feat(timezone): add ZonedTime class

--- include/lines/temporal.h
+++ include/lines/temporal.h
@@ -849,6 +849,8 @@ class DateTime {
     [[nodiscard]] auto time() const -> Timestamp { return Timestamp(_tp.time_since_epoch()); }
 };
 
+// This class represents a fixed offset from UTC.
+// It is NOT equivalent to std::chrono::time_zone.
 class TimeZone {
     Seconds _offset;
 
@@ -878,6 +880,23 @@ class TimeZone {
         return TimeZone(Seconds{local_sec - utc_sec});
 #endif
     }
+};
+
+// Wraps TimePoint and TimeZone into an object representing a zoned time
+class ZonedTime {
+    TimeZone _tz;
+    TimePoint _tp;
+
+  public:
+    ZonedTime(const ZonedTime &) = default;
+    ZonedTime(ZonedTime &&) = default;
+    auto operator=(const ZonedTime &) -> ZonedTime & = default;
+    auto operator=(ZonedTime &&) -> ZonedTime & = default;
+    explicit ZonedTime(TimePoint tp, TimeZone tz) : _tz(tz), _tp(tp) {} // NOLINT
+    ~ZonedTime() = default;
+
+    auto get_local_time() const -> TimePoint { return _tp + _tz.offset(); } // NOLINT
+    auto get_sys_time() const -> TimePoint { return _tp; }                  // NOLINT
 };
 
 struct UTCClock final {
