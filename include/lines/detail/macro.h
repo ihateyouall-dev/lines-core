f1505b59fcc3dad056e773b97c9626fc9e41070c
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-01-07 14:49:09 +0200

    refactor: change version in macros

--- include/lines/detail/macro.h
+++ include/lines/detail/macro.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define LINES_VERSION "0.1.0b"
+#define LINES_VERSION "0.1.0"
 #define LINES_VERSION_MAJOR 0
 #define LINES_VERSION_MINOR 1
 #define LINES_VERSION_PATCH 0
-#define LINES_VERSION_BETA true
+#define LINES_VERSION_BETA false
 #define LINES_VERSION_ALPHA false
 
 #if defined(_WIN32)
