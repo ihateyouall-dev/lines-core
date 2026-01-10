32ed9a13e82f60746d99115a5108174e97cc3f71
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-01-11 00:22:41 +0200

    refactor!: move Year, Month and Day to temporal/ymd.hpp

---
+++ include/lines/temporal/ymd.hpp
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <lines/temporal/duration.hpp>
+
+namespace Lines::Temporal {
+class Year {
+    int _rep;
+
+    using Rep = decltype(_rep);
+
+  public:
+    Year(const Year &) = default;
+    Year(Year &&) = default;
+    auto operator=(const Year &) -> Year & = default;
+    auto operator=(Year &&) -> Year & = default;
+    explicit Year(int rep) : _rep(rep) {}
+    ~Year() = default;
+
+    constexpr explicit operator int() const noexcept;
+
+    auto operator<=>(const Year &) const = default;
+
+    auto operator+() const noexcept -> Year;
+
+    auto operator-() const noexcept -> Year;
+
+    auto operator+=(const Years &yrs) -> Year &;
+
+    auto operator-=(const Years &yrs) -> Year &;
+
+    auto operator++() -> Year &;
+
+    auto operator++(int) -> Year;
+
+    auto operator--() -> Year &;
+
+    auto operator--(int) -> Year;
+
+    auto operator+(const Years &yrs) const -> Year;
+
+    auto operator-(const Years &yrs) const -> Year;
+
+    auto operator-(const Year &year) const -> Years;
+
+    [[nodiscard]] constexpr auto is_leap() const noexcept -> bool;
+
+    [[nodiscard]] auto ok() const noexcept -> bool;
+};
+
+auto operator+(const Years &lhs, const Year &rhs) -> Year;
+
+class Month {
+    // 1 <= _rep <= 12. Not strong invariant
+    uint8_t _rep;
+
+  public:
+    Month(const Month &) = default;
+    Month(Month &&) = default;
+    auto operator=(const Month &) -> Month & = default;
+    auto operator=(Month &&) -> Month & = default;
+    explicit Month(uint32_t rep);
+    ~Month() = default;
+
+    auto operator<=>(const Month &) const = default;
+
+    explicit constexpr operator unsigned() const;
+
+    auto operator++() -> Month &;
+
+    auto operator++(int) -> Month;
+
+    auto operator--() -> Month &;
+
+    auto operator--(int) -> Month;
+
+    auto operator+=(const Months &months) -> Month &;
+
+    auto operator-=(const Months &months) -> Month &;
+
+    auto operator+(const Months &months) const -> Month;
+
+    auto operator-(const Months &months) const -> Month;
+
+    auto operator-(const Month &month) const -> Months;
+
+    [[nodiscard]] auto ok() const -> bool;
+};
+
+auto operator+(const Months &lhs, const Month &rhs) -> Month;
+
+class Day {
+    // 1 <= _rep <= 31. Not strong invariant
+    uint8_t _rep;
+
+  public:
+    Day(const Day &) = default;
+    Day(Day &&) = default;
+    auto operator=(const Day &) -> Day & = default;
+    auto operator=(Day &&) -> Day & = default;
+    explicit Day(uint32_t rep);
+    ~Day() = default;
+
+    auto operator<=>(const Day &) const = default;
+
+    explicit constexpr operator unsigned() const;
+
+    auto operator++() -> Day &;
+
+    auto operator++(int) -> Day;
+
+    auto operator--() -> Day &;
+
+    auto operator--(int) -> Day;
+
+    auto operator+=(const Days &days) -> Day &;
+
+    auto operator-=(const Days &days) -> Day &;
+
+    auto operator+(const Days &days) const -> Day;
+
+    auto operator-(const Days &days) const -> Day;
+
+    auto operator-(const Day &day) const -> Days;
+
+    [[nodiscard]] auto ok() const -> bool;
+};
+
+auto operator+(const Days &lhs, const Day &rhs) -> Day;
+
+enum class Weekday : int8_t {
+    Monday = 0,
+    Tuesday = 1,
+    Wednesday = 2,
+    Thursday = 3,
+    Friday = 4,
+    Saturday = 5,
+    Sunday = 6
+};
+} // namespace Lines::Temporal
---
+++ src/temporal/ymd.cpp
@@ -0,0 +1,188 @@
+#include <lines/temporal/ymd.hpp>
+
+// Year
+constexpr Lines::Temporal::Year::operator int() const noexcept { return _rep; }
+
+auto Lines::Temporal::Year::operator+() const noexcept -> Year { return *this; }
+
+auto Lines::Temporal::Year::operator-() const noexcept -> Year { return Year{-_rep}; }
+
+auto Lines::Temporal::Year::operator+=(const Years &yrs) -> Year & {
+    _rep += static_cast<Rep>(yrs.count());
+    return *this;
+}
+
+auto Lines::Temporal::Year::operator-=(const Years &yrs) -> Year & {
+    _rep -= static_cast<Rep>(yrs.count());
+    return *this;
+}
+
+auto Lines::Temporal::Year::operator++() -> Year & {
+    ++_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Year::operator++(int) -> Year {
+    auto temp = *this;
+    ++*this;
+    return temp;
+}
+
+auto Lines::Temporal::Year::operator--() -> Year & {
+    --_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Year::operator--(int) -> Year {
+    auto temp = *this;
+    --*this;
+    return temp;
+}
+
+auto Lines::Temporal::Year::operator+(const Years &yrs) const -> Year {
+    auto temp = *this;
+    temp += yrs;
+    return temp;
+}
+
+auto Lines::Temporal::Year::operator-(const Years &yrs) const -> Year {
+    auto temp = *this;
+    temp -= yrs;
+    return temp;
+}
+
+auto Lines::Temporal::Year::operator-(const Year &year) const -> Years {
+    return Years{_rep - year._rep};
+}
+
+constexpr auto Lines::Temporal::Year::is_leap() const noexcept -> bool {
+    if (_rep % 400 == 0) { // NOLINT
+        return true;
+    }
+    if (_rep % 100 == 0) { // NOLINT
+        return false;
+    }
+    return _rep % 4 == 0;
+}
+
+auto Lines::Temporal::Year::ok() const noexcept -> bool {
+    return _rep != std::numeric_limits<Rep>::min();
+}
+
+auto Lines::Temporal::operator+(const Years &lhs, const Year &rhs) -> Year { return rhs + lhs; }
+
+// Month
+Lines::Temporal::Month::Month(uint32_t rep) : _rep(rep) {}
+
+constexpr Lines::Temporal::Month::operator unsigned() const { return _rep; }
+
+auto Lines::Temporal::Month::operator++() -> Month & {
+    ++_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Month::operator++(int) -> Month {
+    auto temp = *this;
+    ++*this;
+    return temp;
+}
+
+auto Lines::Temporal::Month::operator--() -> Month & {
+    --_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Month::operator--(int) -> Month {
+    auto temp = *this;
+    --*this;
+    return temp;
+}
+
+auto Lines::Temporal::Month::operator+=(const Months &months) -> Month & {
+    _rep += months.count();
+    return *this;
+}
+
+auto Lines::Temporal::Month::operator-=(const Months &months) -> Month & {
+    _rep -= months.count();
+    return *this;
+}
+
+auto Lines::Temporal::Month::operator+(const Months &months) const -> Month {
+    auto temp = *this;
+    temp += months;
+    return temp;
+}
+
+auto Lines::Temporal::Month::operator-(const Months &months) const -> Month {
+    auto temp = *this;
+    temp -= months;
+    return temp;
+}
+
+auto Lines::Temporal::Month::operator-(const Month &month) const -> Months {
+    return Months{_rep - month._rep};
+}
+
+auto Lines::Temporal::Month::ok() const -> bool {
+    return _rep >= 1 && _rep <= 12; // NOLINT
+}
+
+auto Lines::Temporal::operator+(const Months &lhs, const Month &rhs) -> Month { return rhs + lhs; }
+
+// Day
+Lines::Temporal::Day::Day(uint32_t rep) : _rep(rep) {}
+
+constexpr Lines::Temporal::Day::operator unsigned() const { return _rep; }
+
+auto Lines::Temporal::Day::operator++() -> Day & {
+    ++_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Day::operator++(int) -> Day {
+    auto temp = *this;
+    ++*this;
+    return temp;
+}
+
+auto Lines::Temporal::Day::operator--() -> Day & {
+    --_rep;
+    return *this;
+}
+
+auto Lines::Temporal::Day::operator--(int) -> Day {
+    auto temp = *this;
+    --*this;
+    return temp;
+}
+
+auto Lines::Temporal::Day::operator+=(const Days &days) -> Day & {
+    _rep += days.count();
+    return *this;
+}
+
+auto Lines::Temporal::Day::operator-=(const Days &days) -> Day & {
+    _rep -= days.count();
+    return *this;
+}
+
+auto Lines::Temporal::Day::operator+(const Days &days) const -> Day {
+    auto temp = *this;
+    temp += days;
+    return temp;
+}
+
+auto Lines::Temporal::Day::operator-(const Days &days) const -> Day {
+    auto temp = *this;
+    temp -= days;
+    return temp;
+}
+
+auto Lines::Temporal::Day::operator-(const Day &day) const -> Days { return Days{_rep - day._rep}; }
+
+auto Lines::Temporal::Day::ok() const -> bool {
+    return _rep >= 1 && _rep <= 31; // NOLINT
+}
+
+auto Lines::Temporal::operator+(const Days &lhs, const Day &rhs) -> Day { return rhs + lhs; }
