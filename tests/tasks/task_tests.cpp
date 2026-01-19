b80729f9e343f6817fa43a6552ce1d9148ed9363
ihateyouall-dev <ihateyouallfuckyou@proton.me>
2026-01-19 23:41:01 +0200

    tests(task): rewrite task tests

--- tests/tasks/task_tests.cpp
+++ tests/tasks/task_tests.cpp
@@ -1,36 +1,59 @@
+#include "lines/tasks/task_info.hpp"
 #include <gtest/gtest.h>
 #include <lines/tasks/task.hpp>
 #include <lines/temporal/date.hpp>
 
 using namespace Lines;
 
-TEST(Task, MoveTransfersOwnership) {
-    Task t1 = make_task().info(TaskInfo("title")).task(); // NOLINT
+TEST(TaskAccessors, Getters) {
+    Task task = make_task().info(TaskInfo{"title", "description", {"tag1", "tag2"}}).task();
 
-    Task t2 = std::move(t1); // NOLINT
-    EXPECT_FALSE(t2.title().empty());
+    EXPECT_EQ(task.title(), "title");
+    EXPECT_EQ(task.description().value(), "description");
+    EXPECT_FALSE(task.tags().empty());
 }
 
-TEST(Task, SetTitleThrowsOnEmpty) {
-    Task t = make_task().info(TaskInfo("title")).task(); // NOLINT
+TEST(TaskAccessors, Setters) {
+    Task task = make_task().info(TaskInfo{"title", "description", {"tag1", "tag2"}}).task();
 
-    EXPECT_THROW(t.set_title(""), std::invalid_argument);
+    task.set_title("whatever");
+    task.set_description("whatever");
+    task.set_tags({});
+
+    EXPECT_EQ(task.title(), "whatever");
+    EXPECT_EQ(task.description().value(), "whatever");
+    EXPECT_TRUE(task.tags().empty());
+};
+
+TEST(TaskInvariants, Title) {
+    // Task title cannot be empty, it must have at least 1 character
+    EXPECT_THROW(make_task().info(TaskInfo{""}).task(), std::invalid_argument);
+
+    Task task = make_task().info(TaskInfo{"not empty title"}).task();
+
+    EXPECT_THROW(task.set_title(""), std::invalid_argument);
 }
 
-TEST(TaskFactory, CannotBeReusedAfterMove) {
-    auto factory = make_task().info(TaskInfo("title"));
-    Task t = std::move(factory).task(); // NOLINT
+TEST(TaskSpecialMembers, Copy) {
+    Task task1 = make_task().info(TaskInfo{"first"}).task();
+    Task task2 = make_task().info(TaskInfo{"second"}).task();
 
-    // at this point, factory is no longer valid
-    SUCCEED();
+    task1 = task2;
+
+    Task task3(task2); // NOLINT
+
+    EXPECT_EQ(task1.title(), "second");
+    EXPECT_EQ(task3.title(), "second");
+
+    task2 = task2; // Nothing happens
 }
 
-TEST(TaskFactory, DefaultCompletionIs) {
-    Task t = make_task().info(TaskInfo("title")).task(); // NOLINT
-    EXPECT_FALSE(t.completion().completed());
-}
+TEST(TaskCompletion, ConstAndNonConst) {
+    const Task const_task = make_task().info(TaskInfo{"const"}).task();
+    Task task = make_task().info(TaskInfo{"non const"}).task();
 
-TEST(TaskFactory, AllowsPartialConfiguration) {
-    Task t = make_task().info(TaskInfo("title")).task(); // NOLINT
-    EXPECT_EQ(t.title(), "title");
+    EXPECT_FALSE(const_task.completion().completed());
+    task.completion().complete();
+
+    EXPECT_TRUE(task.completion().completed());
 }
